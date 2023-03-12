fa89d6457e419927b65fb1eedca539417c112639
Edrick Belmiro <edrickbelmiro@gmail.com>
2023-03-12 04:56:53 -0700

    Implemented UI trigger and delay
    UI can be implemented in the FishMarketUI and shopUI. Press E again to go back and close UI, with adjustable delay (default is 1s).

--- Assets/_Project/Features/Shop/FishMarketController.cs
+++ Assets/_Project/Features/Shop/FishMarketController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.Tilemaps;
 
 public class FishMarketController : MonoBehaviour
@@ -9,18 +10,54 @@ public class FishMarketController : MonoBehaviour
     public Tilemap fishMarketTiles;
     public GameObject fishMarketPrompt;
     public GameObject fishMarketUI;
+    public float delay = 1f;
 
     private bool isFishMarketUIOpen;
+    private float delayState = 0f;
+
+    private void Start()
+    {
+        delayState = 0f;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        delayState += Time.deltaTime;
+        fishMarketUI.SetActive(isFishMarketUIOpen);
         Vector2 playerPos = transform.position;
         int playerX = (int)Mathf.Floor(playerPos.x);
         int playerY = (int)Mathf.Floor(playerPos.y);
 
         if (!isFishMarketUIOpen)
             TryTriggerShopUI(playerX, playerY);
+
+
+        if (fishMarketPrompt.activeInHierarchy)
+        {
+            if (Input.GetKey(KeyCode.E) && !isFishMarketUIOpen && delayState >= delay)
+            {
+                fishMarketPrompt.SetActive(false);
+                isFishMarketUIOpen = true;
+                fishMarketUI.SetActive(true);
+                delayState = 0f;
+            }
+        }
+        else if (!fishMarketUI.activeInHierarchy)
+        {
+            isFishMarketUIOpen = false;
+        }
+        else if (fishMarketUI.activeInHierarchy)
+        {
+            if (Input.GetKey(KeyCode.E) && delayState >= delay)
+            {
+                fishMarketPrompt.SetActive(true);
+                isFishMarketUIOpen = false;
+                fishMarketUI.SetActive(false);
+                delayState = 0f;
+            }
+        }
     }
 
     void TryTriggerShopUI(int playerX, int playerY)
--- Assets/_Project/Features/Shop/ShopController.cs
+++ Assets/_Project/Features/Shop/ShopController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using static UnityEditor.Experimental.GraphView.GraphView;
@@ -10,18 +11,52 @@ public class ShopController : MonoBehaviour
     public Tilemap shopTiles;
     public GameObject shopPrompt;
     public GameObject shopUI;
-
+    public float delay = 1f;
+    
     private bool isShopUIOpen;
+    private float delayState;
+
+    private void Start()
+    {
+        delayState = 0f;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        delayState += Time.deltaTime;
+        shopUI.SetActive(isShopUIOpen);
         Vector2 playerPos = transform.position;
         int playerX = (int)Mathf.Floor(playerPos.x);
         int playerY = (int)Mathf.Floor(playerPos.y);
 
         if (!isShopUIOpen)
             TryTriggerShopUI(playerX, playerY);
+
+        if (shopPrompt.activeInHierarchy)
+        {
+            if (Input.GetKey(KeyCode.E) && delayState >= delay && !isShopUIOpen)
+            {
+                shopPrompt.SetActive(false);
+                isShopUIOpen = true;
+                shopUI.SetActive(true);
+                delayState = 0f;
+            }
+        }
+        else if (!shopUI.activeInHierarchy)
+        {
+            isShopUIOpen = false;
+        }
+        else if (shopUI.activeInHierarchy)
+        {
+            if (Input.GetKey(KeyCode.E) && delayState >= delay)
+            {
+                shopPrompt.SetActive(true);
+                isShopUIOpen = false;
+                shopUI.SetActive(false);
+                delayState = 0f;
+            }
+        }
     }
 
     void TryTriggerShopUI(int playerX, int playerY)
